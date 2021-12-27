6635dce85bf8ec0cc0234841a30eec4dcfa010e5
Jishnu Kumar Srivastava <82699906+Jks08@users.noreply.github.com>
2021-12-27 09:36:33 +0530

    Update netmed_ayush_category_data.py

--- NetMeds/netmed_ayush_category_data.py
+++ NetMeds/netmed_ayush_category_data.py
@@ -3,18 +3,12 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:86.0) Gecko/20100101 Firefox/86.0'}
-
 medicinelist=[]
-
 def getMedicine(category_name, page):
     url = 'https://www.netmeds.com/non-prescriptions/ayush/page/{page}'
-
     r = requests.get(url, headers=headers)
     soup = BeautifulSoup(r.text, 'html.parser')
-
     medicines = soup.find_all('div', {'class':'col-md-3 padding-none'})
-    #print(len(medicines))
-
     for item in medicines:
         medicine= {
         'title' : item.find('span', {'class':'clsgetname'}).text,
@@ -24,10 +18,8 @@ def getMedicine(category_name, page):
         }
         medicinelist.append(medicine)
     return
-
 for x in range(1,666):
     getMedicine('category_name', x)
-
 df = pd.DataFrame(medicinelist)  
 print(df) 
 df.to_excel('netmed_ayush_category_data.xlsx')
