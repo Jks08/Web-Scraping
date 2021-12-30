e15c805f61586e545b94258cabad4c1120289b5b
Jishnu Kumar Srivastava <82699906+Jks08@users.noreply.github.com>
2021-12-30 14:29:51 +0530

    Update netmed_ayush_category_data.py

--- NetMeds/netmed_ayush_category_data.py
+++ NetMeds/netmed_ayush_category_data.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:86.0) Gecko/20100101 Firefox/86.0'}
 medicinelist=[]
+
 def getMedicine(category_name, page):
     url = 'https://www.netmeds.com/non-prescriptions/ayush/page/{page}'
     r = requests.get(url, headers=headers)
@@ -18,9 +19,12 @@ def getMedicine(category_name, page):
         }
         medicinelist.append(medicine)
     return
+
 for x in range(1,666):
     getMedicine('category_name', x)
-df = pd.DataFrame(medicinelist)  
-print(df) 
+    df = pd.DataFrame(medicinelist)  
+
+ print(df) 
 df.to_excel('netmed_ayush_category_data.xlsx')
+
 print('Done. The End')
